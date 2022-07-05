7ec082b9b1aa8fe33998e9375fdaffc61976d67c
shreyas-xo <shreyas4120@gmail.com>
2022-07-05 12:23:27 -0400

    Added the index linking to respective entry's page functionality.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown import markdown
 
 from . import util
 
@@ -8,3 +9,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        content= "#Error: Page not found"
+        title='Error' 
+    content = markdown(content)
+    return render(request, "encyclopedia/entry.html", {'content': content,'title':title})
